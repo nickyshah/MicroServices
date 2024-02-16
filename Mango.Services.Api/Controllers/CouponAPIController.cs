0b316601c5203da52a7f29851963776810c32af6
Nicky Shah <nickshah534@gmail.com>
2024-02-16 16:35:22 +1100

    Getting the List

---
+++ Mango.Services.Api/Controllers/CouponAPIController.cs
@@ -0,0 +1,33 @@
+﻿using Mango.Services.CouponApi.Data;
+using Mango.Services.CouponApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponAPIController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+    }
+}
--- Mango.Services.Api/Data/ApplicationDbContext.cs
+++ Mango.Services.Api/Data/ApplicationDbContext.cs
@@ -15,7 +15,21 @@ namespace Mango.Services.CouponApi.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "10OFF",
+                DiscountAmount = 10,
+                MinimumAmount = 20
+            });
 
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "20OFF",
+                DiscountAmount = 20,
+                MinimumAmount = 40
+            });
         }
     }
 }
