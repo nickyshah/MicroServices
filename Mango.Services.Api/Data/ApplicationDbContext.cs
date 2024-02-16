b4547a03c56c73ac69904aa5d2d90598cfcdefe5
Nicky Shah <nickshah534@gmail.com>
2024-02-16 16:06:30 +1100

    Coupon Api initiated and dabase is created and next step is to seed data

---
+++ Mango.Services.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Services.CouponApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
