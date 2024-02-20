d382ac9569dafe60f19afb1f582e2a76d3f874a8
Nicky Shah <nickshah534@gmail.com>
2024-02-20 15:33:04 +1100

    Bootstrap theme and icons added with dropdown in index home page

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,18 @@
+﻿using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            return View();
+        }
+    }
+}
