2bd075b8d87f63d8e2f70ddaedba13ebde7712b1
Nicky Shah <nickshah534@gmail.com>
2024-02-22 11:03:42 +1100

    Toastr is configured and Auth api is configured half way

---
+++ Mango.Services.AuthAPI/Services/Iservices/IAUthServices.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+
+namespace Mango.Services.AuthAPI.Services.Iservices
+{
+    public interface IAuthServices
+    {
+        Task<string> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginRequestDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
