8d5c8a30e6e1069b199d013ee4c2d51148b8b87a
Nicky Shah <nickshah534@gmail.com>
2024-03-07 14:28:35 +1100

    UI updated

--- Mango.Services.OrderAPI/MappingConfig.cs
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -10,13 +10,18 @@ namespace Mango.Services.OrderAPI
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
-                config.CreateMap<OrderHeaderDto, CartHeaderDto>().ForMember(dest => dest.CartTotal, u => u.MapFrom(src => src.OrderTotal)).ReverseMap();
+                config.CreateMap<OrderHeaderDto, CartHeaderDto>()
+                .ForMember(dest => dest.CartTotal, u => u.MapFrom(src => src.OrderTotal)).ReverseMap();
+
                 config.CreateMap<CartDetailsDto, OrderDetailsDto>()
-                    .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
-                    .ForMember(dest => dest.Price, u => u.MapFrom(src => src.Product.Price));
+                .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.Price, u => u.MapFrom(src => src.Product.Price));
+
                 config.CreateMap<OrderDetailsDto, CartDetailsDto>();
+
                 config.CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
-                config.CreateMap<OrderDetails, OrderDetailsDto>().ReverseMap();
+                config.CreateMap<OrderDetailsDto, OrderDetails>().ReverseMap();
+
             });
             return mappingConfig;
         }
