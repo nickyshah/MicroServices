f647423efc6d0d3bdcb123a93ef9ad18a98e895f
Nicky Shah <nickshah534@gmail.com>
2024-03-11 15:24:35 +1100

    Reward Service is configured

---
+++ Mango.Services.RewardAPI/Services/IRewardService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Services.RewardAPI.Message;
+
+namespace Mango.Services.EmailAPI.Services
+{
+    public interface IRewardService
+    {
+        Task UpdateRewards(RewardsMessage rewardsMessage);
+    }
+}
---
+++ Mango.Services.RewardAPI/Services/RewardService.cs
@@ -0,0 +1,39 @@
+﻿using Mango.Services.RewardAPI.Data;
+using Mango.Services.RewardAPI.Message;
+using Mango.Services.RewardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.EmailAPI.Services
+{
+    public class RewardService : IRewardService
+    {
+        private DbContextOptions<AppDbContext> _dbOptions;
+
+        public RewardService(DbContextOptions<AppDbContext> dbOptions)
+        {
+            this._dbOptions = dbOptions;
+        }
+
+        public async Task UpdateRewards(RewardsMessage rewardsMessage)
+        {
+            try
+            {
+                Reward reward = new()
+                {
+                    OrderId = rewardsMessage.OrderId,
+                    RewardsActivity = rewardsMessage.RewardActivity,
+                    userId = rewardsMessage.UserId,
+                    RewardsDate = DateTime.Now
+                };
+                await using var _db = new AppDbContext(_dbOptions);
+                await _db.Rewards.AddAsync(reward);
+                await _db.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
