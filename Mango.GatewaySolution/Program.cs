1cc76aa3c293a21966ba9294d715fb8319bd55f6
Nicky  shah <nickshah534@gmail.com>
2024-03-14 16:09:03 +1100

    packages added and ocelot is configured in program.cs

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,6 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddOcelot();
+
 app.MapGet("/", () => "Hello World!");
+
+app.UseOcelot();
 
 app.Run();
