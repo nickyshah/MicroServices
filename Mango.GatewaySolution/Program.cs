5e4caebb651f28859968b95b3e395a61284cca1d
Nicky  shah <nickshah534@gmail.com>
2024-03-15 12:30:51 +1100

    Ocelot is configured and added in new json file and registered in program.cs

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -4,14 +4,17 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
-
 builder.AddAppAuthentication();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 
 app.Run();
